15da0a0be1f6467a878bfc6f0a4c9000c07642a0
Matthias97 <huetten.matthias@gmail.com>
2016-12-21 13:55:42 +0100

    Änderung title

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Hello World</title>
     </head>
     <body>
         <?php
-        // put your code here
+        // put your code herexhsajdw
         ?>
     </body>
 </html>
