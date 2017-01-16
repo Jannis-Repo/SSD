8b3b898aa4af9d8b5a6981609cedfa8e0246ea6b
J@nn1s <luegering.jannis@googlemail.com>
2017-01-16 10:13:09 +0100

    Cleaning up
    Deleting files

--- footer.php
+++
@@ -1,6 +0,0 @@
-<!-- FOOTER WITH COPYRIGHT -->
-<div class="footer">
-	<div>
-		<strong>Copyright</strong> White Stone &copy; 2016-2017
-	</div>
-</div>
--- scripts.php
+++
@@ -1,9 +0,0 @@
-<!-- SCRIPTS -->
-
-<script src="js/jquery-2.1.1.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/inspinia.js"></script>
-<!-- Morris -->
-<script src="js/morris/raphael-2.1.0.min.js"></script>
-<script src="js/morris/morris.js"></script>
